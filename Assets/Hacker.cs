f0b2b84d3aaa7f1a838294dc34b851e1215ee73a
CanaanGM <canaand1@gmail.com>
2019-07-28 23:04:46 +0300

    Added all the levels
    Ninja for 1st lvl
    Harly for 2nd
    Hungry for 3d
    menu to go to MainMenu

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -15,6 +15,7 @@ public class Hacker : MonoBehaviour
     }
     void ShowGameMenu ()
     {
+        currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
         Terminal.WriteLine("Welcome Mr Wayne ~!");
         Terminal.WriteLine("Who would you like to mess with?");
@@ -23,8 +24,9 @@ public class Hacker : MonoBehaviour
         Terminal.WriteLine("3. The Justice League ~!");
         Terminal.WriteLine("Please Choose: ");
     }
+    
 
-     void OnUserInput(string input)
+    void OnUserInput(string input)
         {
             if( input.ToLower() == "menu")
             {
@@ -36,33 +38,86 @@ public class Hacker : MonoBehaviour
             }
             else if( currentScreen == Screen.Password)
             {
-                MenuController(input);
+            if (level == 1)
+            {
+                print(input);
+                if ( input == "Ninja")
+                {
+                    Terminal.WriteLine("You're in, Good Job~!");
+                    Terminal.WriteLine("Write 'menu' to Return!");
+                }
+                else
+                {
+                    Terminal.WriteLine("Wrong code...");
+                    // after 6 attempts print a lose message and go to mainMenu..
+                }
+            }
+            else if (level == 2)
+            {
+                if (input == "Harly" )
+                {
+                    Terminal.WriteLine("You're in, Good Job~!");
+                    Terminal.WriteLine("Write 'menu' to Return!");
+                }
+                else if( input.ToLower() == "batman")
+                {
+                    Terminal.WriteLine("I've been expecting ya! old freind~!");
+                    // need to do something speical here~!
+
+                }
+                else
+                {
+                    Terminal.WriteLine("Wrong code...");
+                    // after 4 attempts print a lose message and go to mainMenu..
+                }
+            }
+            else if (level == 3)
+            {
+                if (input == "Hungry")
+                {
+                    Terminal.WriteLine("You're in, Good Job~!");
+                    Terminal.WriteLine("Write 'menu' to Return!");
+                }
+                
+                else
+                {
+                    Terminal.WriteLine("Wrong code...");
+                    // after 2 attempts print a lose message and go to mainMenu..
+                }
             }
         }
-    void MenuController (string input)
+        }
+    void MenuController (string input) // to choose what lvl to play ~!
     {
         if ( input == "1")
         {
             level = 1;
-            GameStart(level);
+            GameStart();
         }
         else if( input == "2")
         {
             level = 2;
-            GameStart(level);
+            GameStart();
         }
         else if( input == "3")
         {
             level = 3;
-            GameStart(level);
+            GameStart();
+        }
+        else
+        {
+            Terminal.WriteLine("Please choose a valid input!");
         }
     }
 
-    private void GameStart(int level)
+    private void GameStart()
     {
         currentScreen = Screen.Password;
+        Terminal.ClearScreen();
         Terminal.WriteLine("You've chosen " + level + " Good Luck ~!");
         Terminal.WriteLine("Please enter your password: ");
+
+        
 
     }
 
